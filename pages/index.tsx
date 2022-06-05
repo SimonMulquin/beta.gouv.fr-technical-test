394f266f2932ce9f9d1c8ad13311c1546a8d62b3
Simon Mulquin <me.simonmulquin.pro>
2022-06-05 22:54:47 +0100

    feat: ✨  Get tasks data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,38 @@
-const Home = () => <body>Hello world !</body>;
+import { gql, useQuery } from "@apollo/client";
+
+const homeQuery = gql`
+  query {
+    tasks {
+      id
+      title
+      description
+      status
+    }
+  }
+`;
+
+interface Task {
+  __typename: "Task";
+  id: string;
+  title: string;
+  description: string;
+  status: string;
+}
+
+interface QueryDataType {
+  tasks: Array<Task>;
+}
+
+const Home = () => {
+  const { data, loading, error } = useQuery<QueryDataType>(homeQuery);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) {
+    alert("Something went wrong 😟");
+    return <p>{error.message}</p>;
+  }
+
+  return <body>{JSON.stringify(data.tasks)}</body>;
+};
 
 export default Home;
