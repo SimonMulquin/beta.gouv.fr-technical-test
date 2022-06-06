35c62378374b27ede355a23e7ffe2f12fc35d063
Simon Mulquin <me.simonmulquin.pro>
2022-06-06 23:33:12 +0100

    style: ✨ Implement checkbox

--- components/Task/index.tsx
+++ components/Task/index.tsx
@@ -1,6 +1,34 @@
-import { TaskItem } from "./styled";
-import { TaskType } from "./types";
+import { useMutation } from "@apollo/client";
+import { updateTaskMutation } from "../../graphql/mutations/tasks";
+import { tasksQuery, TaskType } from "../../graphql/queries/tasks";
+import { TaskItem, Checkbox } from "./styled";
 
-const Task = ({ title }: TaskType) => <TaskItem>{title}</TaskItem>;
+const Task = ({ id, title, status }: TaskType) => {
+  const [mutate, { error }] = useMutation(updateTaskMutation, {
+    refetchQueries: [{ query: tasksQuery }, "tasks"],
+  });
+
+  const handleChange = e => {
+    e.stopPropagation();
+    mutate({
+      variables: {data:{ id, status: { DONE: "PLANNED", PLANNED: "DONE" }[status] }},
+    });
+  };
+
+  if (error) {
+    alert(error);
+  }
+
+  return (
+    <TaskItem>
+      {title}
+      <Checkbox
+        type="checkbox"
+        checked={status === "DONE"}
+        onChange={handleChange}
+      />
+    </TaskItem>
+  );
+};
 
 export default Task;
--- components/Task/styled.ts
+++ components/Task/styled.ts
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
 export const TaskItem = styled.li`
+  display: flex;
   width: 100%;
   max-width: 330px;
   margin: 16px 0;
   padding: 12px;
   background-color: #e4e4e4;
   border-radius: 8px;
+  justify-content: space-between; 
+`;
+
+export const Checkbox = styled.input`
+  width: 16px;
+  height: 16px;
 `;
