a7761c9f84594a93c41980039fc746db33ca855f
Simon Mulquin <me.simonmulquin.pro>
2022-06-06 14:27:50 +0100

    feat: ✨ Implement Prisma over graphql api

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,32 +1,7 @@
 export const resolvers = {
-    Query: {
-      tasks: () => {
-        return [
-          {
-            id: '1',
-            title: 'Mock',
-            description: 'Mock',
-            status: 'PLANNED'
-          },
-          {
-            id: '2',
-            title: 'Mock',
-            description: 'Mock',
-            status: 'PLANNED'
-          },
-          {
-            id: '3',
-            title: 'Mockin',
-            description: 'Mockin',
-            status: 'PLANNED'
-          },
-          {
-            id: '4',
-            title: 'On heaven\'s door',
-            description: 'On heaven\'s door',
-            status: 'PLANNED'
-          },
-        ]
-      },
+  Query: {
+    tasks: (_parent, _args, ctx) => {
+      return ctx.prisma.task.findMany();
     },
-  }
+  },
+};
